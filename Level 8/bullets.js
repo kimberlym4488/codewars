b1ca91a9972e8a09b1cb14a9b85fd6981c70e955
Kim (she/hers) windows <kimberlym4488@comcast.net>
2022-06-18 10:49:39 -0700

    bullets and invert with testing

---
+++ Level 8/bullets.js
@@ -0,0 +1,6 @@
+function hero(bullets, dragons) {
+  //Get Coding!
+  // if dragons*2 is less than or equal to bullets, we win!
+  return dragons * 2 <= bullets ? true : false;
+  // if dragons * 2 is MORE than bullets, we lose :(
+}
